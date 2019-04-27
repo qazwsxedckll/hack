08dc26c588f1cbbbaf3994833145678c82d64754
Evilolipop <decai.wang@foxmail.com>
2019-04-27 22:51:28 +0800

    add parser.h

---
+++ parser.h
@@ -0,0 +1,39 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::endl;
+using std::cin;
+using std::cout;
+using std::string;
+
+enum CmdType { A_COMMAND, C_COMMAND, L_COMMAND };
+
+class Parser
+{
+public:
+    Parser(const string& s);
+    //Opens the input file/stream and gets ready to parse it
+    bool hasMoreCommands();
+    //Are there more commands in the input?
+    void advance();
+    //Reads the next command from the input and makes it the current command
+    CmdType commandType();
+    //return the type of currend command
+    string symbol();
+    //Returns the symbol or decimal Xxx of the current command @Xxx or (Xxx).
+    string dest();
+    //Returns the dest mnemonic in the current C-command. 
+    string comp();
+    //Returns the comp mnemonic in the current C-command.
+    string jump();
+    //Returns the jump mnemonic in the current C-command.
+private:
+    std::ifstream file;
+    string current_cmd;
+};
+#endif
+
