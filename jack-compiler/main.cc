a71026e3427d3210e8fe22d2af3e4316a0e2c5ff
Evilolipop <decai.wang@foxmail.com>
2022-06-23 13:45:00 +0800

    add jack_analyzer

--- jack-compiler/main.cc
+++ jack-compiler/main.cc
@@ -2,41 +2,45 @@
 #include "jack_tokenizer.h"
 
 #include <iostream>
-
-#include <dirent.h>
+#include <filesystem>
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    string input;
-    if (argc != 2) {
+    if (argc != 2)
+    {
         cout << "Usage: " << argv[0] << " <input_file>" << endl;
         return -1;
-    } else {
-        input = argv[1];
-        if (IsJackFile(input)) {
-            Jacktokenizer tokenizer(input); 
+    }
+    else
+    {
+        const std::filesystem::path input(argv[1]);
+        if (IsJackFile(input))
+        {
+            Jacktokenizer tokenizer(input);
             return 0;
-        } else {
-            auto dir = opendir(argv[1]);
-            if (dir == NULL) {
-                cout << "cannot open directory: " << argv[1] << endl;
-                return -1;
-            }
+        }
+        else
+        {
+            // auto dir = opendir(argv[1]);
+            // if (dir == NULL) {
+            //     cout << "cannot open directory: " << argv[1] << endl;
+            //     return -1;
+            // }
 
-            if (input.back() == '/') {
-                input = input.substr(0, input.length() - 1);
-            }
-            auto entity = readdir(dir);
-            string file_path;
-            while (entity != NULL) {
-                file_path = input + "/" + entity->d_name;
-                if (IsJackFile(file_path)) {
-                }
-                entity = readdir(dir);
-            }
-            return 0;
+            // if (input.back() == '/') {
+            //     input = input.substr(0, input.length() - 1);
+            // }
+            // auto entity = readdir(dir);
+            // string file_path;
+            // while (entity != NULL) {
+            //     file_path = input + "/" + entity->d_name;
+            //     if (IsJackFile(file_path)) {
+            //     }
+            //     entity = readdir(dir);
+            // }
+            // return 0;
         }
     }
 }
---
+++ jack-compiler/src/jack_analyzer.h
@@ -0,0 +1,4 @@
+#ifndef JACK_ANALYZER_H_
+#define JACK_ANALYZER_H_
+
+#endif // !JACK_ANALYZER_H_
