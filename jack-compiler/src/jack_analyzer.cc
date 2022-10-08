ff9b97c3142ca963531bc426d6ac01cc5fee39b2
eviilolipop <decai.wang@foxmail.com>
2022-10-08 23:09:45 +0800

    add stuff

--- jack-compiler/src/jack_analyzer.cc
+++ jack-compiler/src/jack_analyzer.cc
@@ -28,13 +28,112 @@ int Analyze(const string& file_name)
     if (IsJackFile(input))
     {
         Jacktokenizer tokenizer(input);
-        const std::filesystem::path output(input.filename().replace_extension(".xml"));
-        CompilationEngine engine(input, output); 
+        const std::filesystem::path output(input.relative_path().replace_extension(".xml"));
+        const std::filesystem::path output(input.relative_path().replace_extension("T.xml"));
+        CompilationEngine engine(input, output);
+        cout << "<tokens>" << endl;
         while (tokenizer.hasMoreTokens())
         {
             tokenizer.advance();
+            auto type = tokenizer.tokenType();
+            switch (type)
+            {
+            case TokenType::KEYWORD:
+            {
+                cout << "<keyword> ";
+                auto keyword = tokenizer.keyWord();
+                switch (keyword)
+                {
+                case Keyword::CLASS:
+                    cout << "class";
+                    break;
+                case Keyword::METHOD:
+                    cout << "method";
+                    break;
+                case Keyword::FUNCTION:
+                    cout << "function";
+                    break;
+                case Keyword::CONSTRUCTOR:
+                    cout << "constructor";
+                    break;
+                case Keyword::INT:
+                    cout << "int";
+                    break;
+                case Keyword::BOOLEAN:
+                    cout << "boolean";
+                    break;
+                case Keyword::CHAR:
+                    cout << "char";
+                    break;
+                case Keyword::VOID:
+                    cout << "void";
+                    break;
+                case Keyword::VAR:
+                    cout << "var";
+                    break;
+                case Keyword::STATIC:
+                    cout << "static";
+                    break;
+                case Keyword::FIELD:
+                    cout << "field";
+                    break;
+                case Keyword::LET:
+                    cout << "let";
+                    break;
+                case Keyword::DO:
+                    cout << "do";
+                    break;
+                case Keyword::IF:
+                    cout << "if";
+                    break;
+                case Keyword::ELSE:
+                    cout << "else";
+                    break;
+                case Keyword::WHILE:
+                    cout << "while";
+                    break;
+                case Keyword::RETURN:
+                    cout << "return";
+                    break;
+                case Keyword::K_TRUE:
+                    cout << "true";
+                    break;
+                case Keyword::K_FALSE:
+                    cout << "false";
+                    break;
+                case Keyword::K_NULL:
+                    cout << "null";
+                    break;
+                case Keyword::THIS:
+                    cout << "this";
+                    break;
+                default:
+                    cout << "Unknown keyword(" << int(keyword) << ")" << endl;
+                    exit(1);
+                    break;
+                }
+                cout << " </keyword>" << endl;
+                break;
+            }
+            case TokenType::SYMBOL:
+                cout << "<symbol> " << tokenizer.symbol() << " </symbol>" << endl;
+                break;
+            case TokenType::IDENTIFIER:
+                cout << "<identifier> " << tokenizer.identifier() << " </identifier>" << endl;
+                break;
+            case TokenType::INT_CONST:
+                cout << "<integerConstant> " << tokenizer.intVal() << " </integerConstant>" << endl;
+                break;
+            case TokenType::STRING_CONST:
+                cout << "<stringConstant> " << tokenizer.stringVal() << " </stringConstant>" << endl;
+                break;
+            default:
+                cout << "Unknown token type(" << int(type) << ")" << endl;
+                exit(1);
+                break;
+            }
         }
-        return 0;
+        cout << "</tokens>" << endl;
     }
     else
     {
@@ -55,6 +154,6 @@ int Analyze(const string& file_name)
         //     }
         //     entity = readdir(dir);
         // }
-        return 0;
     }
+    return 0;
 }
